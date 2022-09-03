c77c42b4d000b9ca812ec5e598e3dd2e54ff9e59
CesarAguilar43 <cesaraguilar196@gmail.com>
2022-09-03 00:26:20 -0500

    first development screen version

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
+import '../assets/css/Layout.css';
 
 const Layout = (props) => {
-    return (
-        <React.Fragment>
-            <div className="container-full">
-                <Header />
-                <main>
-                    {props.children}
-                </main>
-                <Footer />
-            </div>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <div className="container-full">
+        <Header />
+        <main>{props.children}</main>
+        <Footer />
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Layout;
--- src/utils/InfoCards.js
+++ src/utils/InfoCards.js
@@ -6,28 +6,53 @@ export const experienceCards = [
   {
     ruteImg: seridma,
     alt: 'Seridma',
-    titleImg: 'Seridma.com.mx',
+    company: 'Seridma, S de RL de CV.',
     descImg: 'App desarrollada con ReactJS, Css, javascript, react-bootstrap, git-github, npm',
+    city: 'Altamira, Tamaulipas.',
+    site: 'https://www.seridma.com.mx',
     style: 'img1',
     developId: '1',
+    keyWords: {
+      frontEnd: 'React, NodeJs, React-Bootstrap, Font-awesome, Css3',
+      backEnd: '---',
+      moreTools: 'Git, GitHub, Hostinguer',
+    },
+    primaryTools: ['React', 'Nodejs', 'React-Bootstrap'],
   },
   {
     ruteImg: dieselMarket,
     alt: 'TheDieselMarket',
-    titleImg: 'TheDieselMarket.com',
+    company: 'TheDieselMarket.com',
     descImg:
       'App desarrollada con ReactJS, Sass, javascript, material-ui. Api desarrollada con Graphql, nodejs, JWS, Google Cloud Platform, Postgres, npm, git-github.',
+    city: 'Monterrey, Nuevo Leon.',
+    site: 'https://web-dev.thedieselmarket.com/',
     style: 'img2',
     developId: '2',
+    keyWords: {
+      frontEnd: 'React, Material-ui, Css3, sass, JavaScript, Redux',
+      backEnd: 'Express, Graphql, JavaScript, Sequelize',
+      moreTools:
+        'Git, GitHub, NodeJs, npm, yarn, Circle-ci, Google Cloud Platform, jwt, PostgreSQL',
+    },
+    primaryTools: ['Graphql', 'Nodejs', 'Express', 'PostgreSQL', 'Sequelize'],
   },
   {
     ruteImg: momentoGlobal,
     alt: 'Momento Global',
-    titleImg: 'Momento Global',
+    company: 'Momento Global',
     descImg:
       'App desarrollada con React native, Graphql-Apollo-client. Api desarrollada con Graphql, nodejs, Typescript, Nestjs, Google Cloud Platform, npm, git-github, JWT.',
+    city: 'New York, USA.',
+    site: 'https://play.google.com/store/apps/details?id=com.dbfinancial.wallet&hl=es_MX',
     style: 'img3',
     developId: '3',
+    keyWords: {
+      frontEnd: 'React native, JavaScript, Redux',
+      backEnd: 'Express, Graphql, JavaScript, Nestjs, TypeORM, Sequelize',
+      moreTools: 'Git, GitHub, NodeJs, npm, Circle-ci, Google Cloud Platform, jwt, PostgreSQL',
+    },
+    primaryTools: ['Graphql', 'Nestjs', 'Nodejs', 'Express', 'PostgreSQL'],
   },
 ];
 
@@ -51,3 +76,6 @@ export const knowledgeCards = [
       'Tesis presentada para la obtención de grado en master en ciencias de la computación con el nombre de "DESARROLLO DE ARQUITECTURA DE SOPORTE A LA TOMA DE DECISIONES PARA AGENTES EN HARDWARE"',
   },
 ];
+
+export const profile =
+  ' Mi nombre es César Alejandro Aguilar Rodríguez. Tengo tres años desarrollando software trabajando con tecnologías como NodeJS, Java y React tanto con Arquitectura REST y Graphql. Soy una persona con pasión por la enseñanza, empatía, con actitud positiva y ganas de aprender nuevas tecnologías.';
