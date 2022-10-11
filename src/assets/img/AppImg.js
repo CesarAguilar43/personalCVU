20006784fe66e6510db6c0cfa6a3cbe24b2f496c
CesarAguilar43 <cesaraguilar196@gmail.com>
2022-10-10 19:26:27 -0500

    Add new style to development screen

---
+++ src/assets/img/AppImg.js
@@ -0,0 +1,13 @@
+import SeridmaHome from './seridma/seridma-home.png';
+import SeridmaMiddle from './seridma/seridma-middle.png';
+import SeridmaFooter from './seridma/seridma-footer.png';
+import SeridmaWap from './seridma/seridma-wap.png';
+import MomentoPlayStore from './momento-global/momento-play-store.png';
+
+export const IMAGES = {
+  SeridmaHome,
+  SeridmaMiddle,
+  SeridmaFooter,
+  SeridmaWap,
+  MomentoPlayStore,
+};
